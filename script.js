20952e1c7b51dac4cac500d13eb432e3cb25b442
nattane-costa <professoranattane@gmail.com>
2025-03-21 07:12:37 -0300

    calculadora 2.0

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-function calcularIMC(){
-    
+function calcularIMC(event, tipo){
+    event.preventDefault();
+
     let peso = document.getElementById("peso").value;
     let altura = document.getElementById("altura").value;
 
@@ -7,10 +8,26 @@ function calcularIMC(){
 
     let imc = peso / (altura * altura);
 
-    alert(imc);
+    imc = imc.toFixed(2);
+
+    mensagem_final = "Seu IMC é de " + imc + ". "; 
     
     if(imc < 18.5)
-        alert("abaixo do peso");
-    else alert("peso normal ou acima do peso")
+        mensagem_final += "Você está abaixo do peso.";
+    else if(imc < 25)
+         mensagem_final += "Você está saudável.";
+    else if(imc < 30)
+         mensagem_final += "Você está com sobrepeso.";
+    else if(imc < 35)
+         mensagem_final += "Você está com Obesidade Grau I.";
+    else if(imc < 40)
+         mensagem_final += "Você está com Obesidade Grau II.";
+    else  mensagem_final += "Você está com Obesidade Grau III.";
+
+    if(tipo == 1) alert(mensagem_final);
+    else if(tipo == 2) 
+        document.getElementById("resultado").
+    textContent = mensagem_final;
+        else console.log(mensagem_final);
 
 };
